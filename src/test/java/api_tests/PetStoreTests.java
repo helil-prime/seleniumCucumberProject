efd874755ce5a3b1a3666575599037a42120d4ec
helil-primetech <helil@primetechschool.com>
2021-07-08 15:53:14 -0400

    created post put delete functions

--- src/test/java/api_tests/PetStoreTests.java
+++ src/test/java/api_tests/PetStoreTests.java
@@ -1,12 +1,45 @@
 package api_tests;
 
-import static io.restassured.RestAssured.given;
-
+import static io.restassured.RestAssured.*;
 import static org.testng.Assert.*;
+import static org.hamcrest.Matchers.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.jsoup.select.Evaluator.ContainsData;
 import org.testng.annotations.Test;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import io.restassured.RestAssured;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.filter.Filter;
 import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import io.restassured.http.Cookies;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.http.Method;
+import io.restassured.internal.support.FileReader;
+import io.restassured.mapper.ObjectMapper;
+import io.restassured.mapper.ObjectMapperType;
 import io.restassured.response.Response;
+import io.restassured.specification.AuthenticationSpecification;
+import io.restassured.specification.MultiPartSpecification;
+import io.restassured.specification.ProxySpecification;
+import io.restassured.specification.RedirectSpecification;
+import io.restassured.specification.RequestLogSpecification;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 
 public class PetStoreTests {
 
@@ -129,29 +162,14 @@ public class PetStoreTests {
 	 */
 
 	@Test
-	public void postNewPet() {
+	public void postNewPet() throws IOException {
 		 baseUrl = "https://petstore.swagger.io/v2/pet";
-		    requestBody = "{\r\n"
-		    		+ "  \"id\": 22303,\r\n"
-		    		+ "  \"category\": {\r\n"
-		    		+ "    \"id\": 6,\r\n"
-		    		+ "    \"name\": \"sheperd\"\r\n"
-		    		+ "  },\r\n"
-		    		+ "  \"name\": \"booboo\",\r\n"
-		    		+ "  \"photoUrls\": [\r\n"
-		    		+ "    \"string\"\r\n"
-		    		+ "  ],\r\n"
-		    		+ "  \"tags\": [\r\n"
-		    		+ "    {\r\n"
-		    		+ "      \"id\": 10029,\r\n"
-		    		+ "      \"name\": \"DC\"\r\n"
-		    		+ "    }\r\n"
-		    		+ "  ],\r\n"
-		    		+ "  \"status\": \"available\"\r\n"
-		    		+ "}";
+				 
+				 requestBody = "{\"id\":22303,\"category\":{\"id\":6,\"name\":\"sheperd\"},\"name\":\"booboo\",\"photoUrls\":[\"string\"],\"tags\":[{\"id\":10029,\"name\":\"DC\"}],\"status\":\"available\"}";
+				 
 		    
 		    response = given().contentType(ContentType.JSON).body(requestBody)
-		    		.post(baseUrl).andReturn();
+		    		.post(baseUrl);
 		    
 		    int scode = response.statusCode();
 			String contentType = response.getContentType();
@@ -166,8 +184,128 @@ public class PetStoreTests {
 			assertEquals(id, 22303);
 			assertEquals(petName, "booboo");
 			assertEquals(status, "available");
+			
+			System.out.println("Response body as String: " + response.getBody().asString());
+			System.out.println("Response body as Pretty String: " + response.getBody().asPrettyString());
 
 	}
+	
+	
+	// New add ons 
+	
+	
+	@Test
+	public void postNewPetWithChainValidation() throws IOException {
+		 baseUrl = "https://petstore.swagger.io/v2/pet";
+		    
+		 
+		 requestBody = "{\"id\":22303,\"category\":{\"id\":6,\"name\":\"sheperd\"},\"name\":\"booboo\",\"photoUrls\":[\"string\"],\"tags\":[{\"id\":10029,\"name\":\"DC\"}],\"status\":\"available\"}";
+		 
+		   
+		 given().contentType(ContentType.JSON).body(requestBody)
+		    		.post(baseUrl)
+		    		.then()
+		    		.assertThat()
+		    		.body("id", equalTo(22303))
+		    		.and().body("category.id", equalTo(6))
+		    		.and().body("name", is("booboo"))
+		    		.and().body("tags[0].name", equalTo("DC"));
+		    
+//		    int scode = response.statusCode();
+//			String contentType = response.getContentType();
+//			System.out.println("content type :" + contentType);
+//			System.out.println("status code :" + scode);
+//			assertEquals(scode, 200);
+//			assertEquals(contentType, "application/json");
+//			
+//			int id = response.path("id");
+//			String petName = response.path("name");
+//			String status = response.path("status");
+//			assertEquals(id, 22303);
+//			assertEquals(petName, "booboo");
+//			assertEquals(status, "available");
+//			
+//			System.out.println("Response body as String: " + response.getBody().asString());
+//			System.out.println("Response body as Pretty String: " + response.getBody().asPrettyString());
+			
+			//response.then().body("tags.name", equalTo("[DC]"));
+
+	}
+	
+	
+	@Test
+	public void postNewPetWithFile() throws IOException {
+		 baseUrl = "https://petstore.swagger.io/v2/pet";
+				 
+				 File postRequest = new File("./src/test/resources/jsonFiles/createPetRequest.json");
+				 
+
+				 String content = new String(Files.readAllBytes(Paths.get("./src/test/resources/jsonFiles/createPetRequest.json")));
+				 
+				System.out.println(" file content: "+ content);
+		    
+		    response = given().contentType(ContentType.JSON).body(postRequest)
+		    		.post(baseUrl);
+		    
+		    int scode = response.statusCode();
+			String contentType = response.getContentType();
+			System.out.println("content type :" + contentType);
+			System.out.println("status code :" + scode);
+			assertEquals(scode, 200);
+			assertEquals(contentType, "application/json");
+			
+			int id = response.path("id");
+			String petName = response.path("name");
+			String status = response.path("status");
+			assertEquals(id, 22303);
+			assertEquals(petName, "booboo");
+			assertEquals(status, "available");
+			
+			System.out.println("Response body as String: " + response.getBody().asString());
+			System.out.println("Response body as Pretty String: " + response.getBody().asPrettyString());
+			
+			//response.then().body("tags.name", equalTo("[DC]"));
+
+	}
+	
+	
+	@Test
+	public void postNewPetWithFile2() throws IOException {
+		 baseUrl = "https://petstore.swagger.io/v2/pet";
+				 
+				 File postRequest = new File("./src/test/resources/jsonFiles/createPetRequest2.json");
+				 
+
+				 String content = new String(Files.readAllBytes(Paths.get("./src/test/resources/jsonFiles/createPetRequest2.json")));
+				 
+				System.out.println(" file content: "+ content);
+		    
+		    response = given().contentType(ContentType.JSON).body(postRequest)
+		    		.post(baseUrl);
+		    
+		    response.getBody().prettyPrint();
+		    
+		    int scode = response.statusCode();
+			String contentType = response.getContentType();
+			System.out.println("content type :" + contentType);
+			System.out.println("status code :" + scode);
+			assertEquals(scode, 200);
+			assertEquals(contentType, "application/json");
+			
+			int id = response.path("id");
+			String petName = response.path("name");
+			String status = response.path("status");
+			assertEquals(id, 23333);
+			assertEquals(petName, "booboo");
+			assertEquals(status, "available");
+			
+			//System.out.println("Response body as String: " + response.getBody().asString());
+			//System.out.println("Response body as Pretty String: " + response.getBody().asPrettyString());
+			
+			//response.then().body("tags.name", equalTo("[DC]"));
+
+	}
+	
 
 	/*
 	 * Scenario: As a user, I should not be able to perform POST request to invalid data structure
@@ -183,26 +321,8 @@ public class PetStoreTests {
 	public void postNewPetWithInvalidRequestBody() {
 		
 		baseUrl = "https://petstore.swagger.io/v2/pet";
-	    requestBody = "{\r\n"
-	    		+ "  \"i\" 22303,\r\n"
-	    		+ "  \"category\": {\r\n"
-	    		+ "    \"id\": 6,\r\n"
-	    		+ "    \"name\": \"sheperd\"\r\n"
-	    		+ "  },\r\n"
-	    		+ "  \"name\": \"booboo\",\r\n"
-	    		+ "  \"photoUrls\": [\r\n"
-	    		+ "    \"string\"\r\n"
-	    		+ "  ],\r\n"
-	    		+ "  \"tags\": [\r\n"
-	    		+ "    {\r\n"
-	    		+ "      \"id\": 10029,\r\n"
-	    		+ "      \"name\": \"DC\"\r\n"
-	    		+ "    }\r\n"
-	    		+ "  ],\r\n"
-	    		+ "  \"status\": \"available\"\r\n"
-	    		+ "}";
-	    
-	    response = given().contentType(ContentType.JSON).body(requestBody)
+		 File postRequest = new File("./src/test/resources/jsonFiles/invalidAddPetRequestBody.json");
+	    response = given().contentType(ContentType.JSON).body(postRequest)
 	    		.post(baseUrl).andReturn();
 	    
 	    response.getBody().prettyPrint();
@@ -211,10 +331,56 @@ public class PetStoreTests {
 		String contentType = response.getContentType();
 		System.out.println("content type :" + contentType);
 		System.out.println("status code :" + scode);
-		assertEquals(scode, 400);
+		assertEquals(scode, 500);
 		assertEquals(contentType, "application/json");
-		
-		
 
 	}
+	
+	
+	// put mehods
+	
+	@Test
+	public void putRequest() {
+		
+		baseUrl = "https://petstore.swagger.io/v2/pet";
+		 File putRequest = new File("./src/test/resources/jsonFiles/putRequestBody.json");
+	    response = given().contentType(ContentType.JSON).body(putRequest)
+	    		.put(baseUrl).andReturn();
+	    
+	    response.getBody().prettyPrint();
+	    
+	    int scode = response.statusCode();
+		String contentType = response.getContentType();
+		System.out.println("content type :" + contentType);
+		System.out.println("status code :" + scode);
+		assertEquals(scode, 200);
+		assertEquals(contentType, "application/json");
+
+	}
+	
+	//delete method
+	
+	@Test
+	public void deleteRequest() {
+		int petId = 22303;
+		baseUrl = "https://petstore.swagger.io/v2/pet/";
+		
+	    response = given().header("api_key", "newpet").accept("application/json")
+	    		.when()
+	    		.delete(baseUrl + petId).andReturn();
+	    
+	    response.getBody().prettyPrint();
+	    
+	    int scode = response.statusCode();
+		String contentType = response.getContentType();
+		System.out.println("content type :" + contentType);
+		System.out.println("status code :" + scode);
+		assertEquals(scode, 200);
+		assertEquals(contentType, "application/json");
+		
+		assertEquals(response.getBody().path("message"), petId + "");
+
+	}
+	
+	
 }
