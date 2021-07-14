0340d39206ebb51bfafb217980db85eec4d39da4
helil-primetech <helil@primetechschool.com>
2021-07-14 16:15:14 -0400

    added saucelabs integration

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 		features="./src/test/resources/features",
 		glue="step_definitions",
 		dryRun=false,
-		tags="@addTrade"
+		tags="@SauceInvalidLogin"
 		)
 public class TestRunner {
 	
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,13 +1,16 @@
 package utilities;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
-
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
 import io.github.bonigarcia.wdm.managers.InternetExplorerDriverManager;
@@ -20,6 +23,8 @@ public class Driver {
 	 * And if I want to run my script on different browser, 
 	 * all I have to do is change the browser name in the properties file.
 	 */
+	
+	private static final String sauceurl = "https://helil-prime:a3662504-404e-4230-b888-0603405a7ca6@ondemand.us-west-1.saucelabs.com:443/wd/hub";
 	
 	private static WebDriver driver;
 	public static WebDriver getDriver() {
@@ -44,6 +49,9 @@ public class Driver {
 				ChromeDriverManager.chromedriver().setup();
 				driver = new ChromeDriver();
 				break;
+			case "saucelabs":
+				sauceSetup(sauceurl, "Windows 7", "ie", "latest");
+				break;
 			case "headless":
 			default:
 				ChromeDriverManager.chromedriver().setup();
@@ -53,6 +61,20 @@ public class Driver {
 			}
 		}
 		return driver;
+	}
+	
+	
+	public static void sauceSetup(String url, String platform, String browser, String browserVersion) {
+		DesiredCapabilities capability = new DesiredCapabilities();
+		capability.setCapability("platform", platform);
+		capability.setCapability("browserName", browser);
+		capability.setCapability("version", browserVersion);
+		try {
+			driver = new RemoteWebDriver(new URL(url), capability);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	public static void quitDriver() {
