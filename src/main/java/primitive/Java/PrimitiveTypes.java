497d8101ff781b4e6c250b6db42cbc3844ef2b94
elysium306 <elysium306@hotmail.com>
2022-05-03 01:48:25 -0500

    added some primitiveTypes

---
+++ src/main/java/primitive/Java/PrimitiveTypes.java
@@ -0,0 +1,13 @@
+package primitive.Java;
+
+public class PrimitiveTypes {
+	public static void main(String[] args) {
+		String name = "Alim";
+		int number = 123;
+		boolean bool1 = true;
+		boolean bool2 = false;
+		
+		System.out.println(name + " is: "+ bool1 + "\n" + number + " is: " + bool2);
+	}
+
+}
