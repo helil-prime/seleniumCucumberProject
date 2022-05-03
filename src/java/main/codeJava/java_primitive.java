971b4519a173e57033037ac2b9ec1a1d72098b3b
elysium306 <elysium306@hotmail.com>
2022-05-03 01:14:20 -0500

    primitives in Java added

---
+++ src/java/main/codeJava/java_primitive.java
@@ -0,0 +1,10 @@
+public class java_primitive {
+   public static void main(String[] args) {
+       String name = "Alim";
+       int studentNO = 123;
+       boolean bool1 = true;
+       boolean bool2 = false;
+
+       System.out.println(name + "studentNO" + bool1 + bool2);
+   } 
+}
